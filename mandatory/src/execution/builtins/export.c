8b46e92ac31d62cb92215a4bfbf39cfe76ea0206
Reda ben Aissa <137452885+Redabenaissa6@users.noreply.github.com>
2025-08-04 04:20:25 +0000

    ft_export : DONE

--- mandatory/src/execution/builtins/export.c
+++ mandatory/src/execution/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: rben-ais <rben-ais@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 15:22:30 by rben-ais          #+#    #+#             */
-/*   Updated: 2025/08/02 18:39:43 by rben-ais         ###   ########.fr       */
+/*   Updated: 2025/08/04 04:19:09 by rben-ais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static void	print_export_format(t_env *env)
 {
 	while (env)
 	{
-		if(env->value)
+		if (env->value)
 			printf("declare -x %s=\"%s\"\n", env->key, env->value);
 		else
 			printf("declare -x %s\n", env->key);
@@ -24,9 +24,66 @@ static void	print_export_format(t_env *env)
 	}
 }
 
+static int	is_valid_identifier(char *str)
+{
+	int i;
+	
+	if (!str || !*str)
+		return (0);
+	if (!ft_isalpha(str[0]) && str[0] != '_')
+		return (0);
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	handle_assignment(char *key, char *value, t_env **env)
+{
+	t_env	*current;
+	t_env	*new_node;
+
+	current = *env;
+	while (current)
+	{
+		if (!ft_strcmp(current->key, key))
+		{
+			if (current->value)
+				free(current->value);
+			current->value = ft_strdup(value);
+		}
+		current = current->next;
+	}
+	new_node = ft_lstnew_env(ft_strdup(value), ft_strdup(key));
+	if (new_node)
+		ft_lstadd_back_env(env, new_node);
+}
+
 static void	export_variable(char *arg, t_env **env)
 {
+	char	*equal_sign_string;
+	char	*key;
+	char	*value;
 	
+	equal_sign_string = ft_strchr(arg, '=');
+	if (equal_sign_string)
+	{
+		key = ft_substr(arg, 0, equal_sign_string - arg);
+		value = ft_strdup(equal_sign_string + 1);
+		if (is_valid_identifier(key))
+			handle_assignment(key, value, env);
+		else
+			printf("Shellnobyl: export: `%s': not a valid identifier\n", arg);
+		free(key);
+		free(value);
+	}
+	else
+		if (!is_valid_identifier(arg))
+			printf ("Shellnobyl: export: `%s': not a valid identifier", arg);
 }
 
 void	ft_export(char **args, t_env **env)
@@ -37,7 +94,7 @@ void	ft_export(char **args, t_env **env)
 		return;
 	if (!args[1])
 	{
-		printf_export_format(*env);
+		print_export_format(*env);
 		return;
 	}
 	i = 1;
